a6fea7ccd10d501d3321d1b5454c184f5d38ee9a
desha1006055 <ronal17v@gmail.com>
2024-10-07 17:29:35 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  bot: {
+    owners: ["1085112726164086806","1227993330072424450"],  // اونر
+    botID: "1292085473661157417",    // ايدي البوت
+    GuildId: "1291999273575120908",   // ايدي السيرفير
+    ClientId: "1292085473661157417",    // ايدي البوت
+    serverinvte: "https://discord.gg/VQjYqkGn7f", // انفايت سير
+    clientSECRET: "uSUsoFuF3yt77ILBV8IiBZP7kWqqIiIe", // سكريت
+    callbackURL: "https://bec31d21-c959-4190-bf26-91ee7adf8675-00-3abxsvfo8s163.sisko.replit.dev/login", // الكال باك
+    TheLinkVerfy : "https://discord.com/oauth2/authorize?client_id=1292085473661157417&response_type=code&redirect_uri=https%3A%2F%2Fbec31d21-c959-4190-bf26-91ee7adf8675-00-3abxsvfo8s163.sisko.replit.dev%2Flogin&scope=identify+guilds+email+guilds.join", // رابط اوثو رايز بالصلاحيه ادخال الي سيرفرات
+    prefix : '$', 
+     TOKEN: ("MTI5MjA4NTQ3MzY2MTE1NzQxNw.GVZRLm.mByAvLlYvtL21g2dXUlOryG-gv1obCMJZJuJ1Y"),// توكن 
+    TraId  : '1272174323620253746', // الي يتحوله كريديت
+    done : "1292152874326949950",//ايدي روم تمت العملية
+    line : "https://media.discordapp.net/attachments/1292051271234093107/1292152563315249235/401965d2ef6ddc4f.webp?ex=6702b25a&is=670160da&hm=099d9d4391aef79c05e369c0eb83e4d403efd74791f4d8d0131422bb7bc6b84b&=&format=webp",//رابط خط السيرفر
+    stockimg : "https://media.discordapp.net/attachments/1292051271234093107/1292152566653653043/image_5.webp?ex=6702b25a&is=670160da&hm=a1338e93624d034157283b089a28fdbd9e19717be4b9b59ab2542df15b3d51a7&=&format=webp",//رابط صوره ستوك الاعضاء
+      supportimg : "https://media.discordapp.net/attachments/1292051271234093107/1292152566653653043/image_5.webp?ex=6702b25a&is=670160da&hm=a1338e93624d034157283b089a28fdbd9e19717be4b9b59ab2542df15b3d51a7&=&format=webp",//رابط صوره تذكرة الدعم الفني
+    ticketimg : "https://media.discordapp.net/attachments/1292051271234093107/1292152566653653043/image_5.webp?ex=6702b25a&is=670160da&hm=a1338e93624d034157283b089a28fdbd9e19717be4b9b59ab2542df15b3d51a7&=&format=webp",//زابط صور تذكرة الشراء
+    fedroom : "1292008483939549255",//ايدي روم الفيدباك
+    clientrole : "1292009246942302289"//ايدي رتبه الزباين
+  },
+  website: {
+    PORT: "3001",
+  }
+}
